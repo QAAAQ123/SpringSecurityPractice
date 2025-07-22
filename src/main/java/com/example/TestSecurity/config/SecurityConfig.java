d51f9d46e4d9cc058bd9af7a5f4964d41e34dccb
김재원 <wodnjs9058@naver.com>
2025-07-22 15:38:15 +0900

    Config class BCrypt 암호화 method 추가 And MySQL Connection

--- src/main/java/com/example/TestSecurity/config/SecurityConfig.java
+++ src/main/java/com/example/TestSecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //spring boot의 config로 등록
@@ -46,5 +47,11 @@ public class SecurityConfig {
 
         return http.build(); //최종적으로 빌드해서 리턴해준다-> 접근 권한 설정 완료
     }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
